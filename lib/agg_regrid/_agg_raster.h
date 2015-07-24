b5df237cfb5ec6c3db89b697e25b8991b0c0b890
Bill Little <bill.james.little@gmail.com>
2015-07-24 10:25:07 +0100

    Experimental area-weighted regridding using AGG

---
+++ lib/agg_regrid/_agg_raster.h
@@ -0,0 +1,29 @@
+/*
+# (C) British Crown Copyright 2015, Met Office
+#
+# This file is part of iris-extras.
+#
+# iris-extras is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# iris-extras is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with iris-extras.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _AGG_RASTER_H
+#define _AGG_RASTER_H
+
+#include <stdint.h>
+
+
+void _raster(uint8_t *weights, const double *xi, const double *yi,
+             int nx, int ny);
+
+#endif
