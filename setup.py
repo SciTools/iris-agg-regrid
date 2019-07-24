ec3469de1762c0dcf33bd1b883f1db8e34ecb1d7
Bill Little <bill.james.little@gmail.com>
2019-07-24 17:06:45 +0100

    update travis

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ args = dict(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: GIS',
         ],
